e792a852c6d57a783650acae5c089deb29a30f65
Akhil Waghmare <akhilwaghmare@gmail.com>
2023-09-23 18:33:56 -0700

    Starter code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,16 @@
-import SmoothScroll from "@/components/SmoothScroll";
 import Image from "next/image";
 
 export default function Home() {
   const items = Array.from({ length: 20 }, (_, i) => i + 1);
 
   return (
-    <SmoothScroll>
-      <main className="w-[90%] max-w-3xl mx-auto py-10">
-        <section className="flex flex-col gap-10">
-          {items.map((index) => (
-            <ListItem key={index} index={index} />
-          ))}
-        </section>
-      </main>
-    </SmoothScroll>
+    <main className="w-[90%] max-w-3xl mx-auto py-10">
+      <section className="flex flex-col gap-10">
+        {items.map((index) => (
+          <ListItem key={index} index={index} />
+        ))}
+      </section>
+    </main>
   );
 }
 
